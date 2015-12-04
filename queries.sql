e6a0701d0ba0e2458f328a7097a3e2bbc5299858
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-12-04 11:34:17 +0000

    (#20) Replace "Natural Join" with ","
    Closes #20

--- queries.sql
+++ queries.sql
@@ -1,10 +1,12 @@
 # Query 2a
 SELECT userid
-FROM utilizador NATURAL JOIN login
-GROUP BY userid
+FROM utilizador, login
+WHERE utilizador.userid = login.userid
+GROUP BY utilizador.userid
 HAVING count(CASE WHEN sucesso = 1 THEN 1 END) < count(contador_login) - count(CASE WHEN sucesso = 1 THEN 1 END);
 
-SELECT userid
-FROM utilizador NATURAL JOIN login
-GROUP BY userid
+SELECT utilizador.userid
+FROM utilizador, login
+WHERE utilizador.userid = login.userid
+GROUP BY utilizador.userid
 HAVING sum(sucesso = 1) < count(contador_login) - sum(sucesso = 1); 
