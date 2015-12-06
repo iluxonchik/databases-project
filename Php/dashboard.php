53b17abb1e483f97e856f599f4f4a45f381a65c8
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-12-06 01:29:00 +0000

    (#28) Implemented user login/logout and general template.
    Implmeneted basic user login and logout functionality (it was a
    quick write-up, just so that it does the job).
    
    Style fixed (tabs to spaces, new line at the end of files).
    
    Closes #28. Closes #25.

--- Php/dashboard.php
+++ Php/dashboard.php
@@ -1,3 +1,13 @@
+<?php 
+require_once('appfunctions.php');
+
+if(is_logged_in()) {
+    show_header();
+} else {
+    redirect_to_login();
+}
+?>
+
 <!-- Just a template, do actual design -->
 <a href="#"> Insert new page</a> <br/>
 <a href="#"> Insert new registry</a> <br/>
