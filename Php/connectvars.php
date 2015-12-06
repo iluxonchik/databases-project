a53f0cf8ecebc07943334015c362c6758e632d26
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-12-06 01:41:46 +0000

    Remove hardcoded vars.

--- Php/connectvars.php
+++ Php/connectvars.php
@@ -1,10 +1,6 @@
 <?php
-$_ENV["DB_HOST"] = 'localhost';
-$_ENV["DB_NAME"] = 'project';
-$_ENV["DB_USER"] = 'root';
-$_ENV["DB_PASSWORD"] = 'johnterry';
-    define('DB_HOST', $_ENV["DB_HOST"]);
-    define('DB_NAME', $_ENV["DB_NAME"]);
-    define('DB_USER', $_ENV["DB_USER"]);
-    define('DB_PASSWORD', $_ENV["DB_PASSWORD"]);
+define('DB_HOST', $_ENV["DB_HOST"]);
+define('DB_NAME', $_ENV["DB_NAME"]);
+define('DB_USER', $_ENV["DB_USER"]);
+define('DB_PASSWORD', $_ENV["DB_PASSWORD"]);
 ?>
