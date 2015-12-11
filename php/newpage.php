f78f8cef4ba567ccc1f3c9544254c3e76aaede12
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2015-12-11 05:19:45 +0000

    updated newpage.php
    - using PDO::beginTransaction in newpage.php
    - moved update_sequencia_table to appfunctions.php

--- php/newpage.php
+++ php/newpage.php
@@ -27,54 +27,33 @@ function update_page_table($dbh, $page_name, $cnt_seq, $is_active) {
         // Table is empty, use id 1
         $page_cnt = 1;
     }
-    
+
     $query = "INSERT INTO pagina (userid, pagecounter, nome, idseq, ativa) VALUES (?, ?, ?, ?, ?);";
     $sth = $dbh->prepare($query);
     $sth->execute(array(get_logged_in_userid(), $page_cnt, $page_name , $cnt_seq, $is_active));
 }
 
-function update_sequencia_table($dbh) {
-        $query = "SELECT contador_sequencia FROM sequencia ORDER BY contador_sequencia DESC LIMIT 1";
-        $sth = $dbh->prepare($query);
- 
-        $sth->execute();
-        if($sth->rowCount()) {
-            $row = $sth->fetch(PDO::FETCH_ASSOC);
-            $cnt_seq = $row['contador_sequencia'] + 1;
-        } else {
-            // Table empty, use id 1
-            $cnt_seq = 1;
-        }
-        
-        $query = "INSERT INTO sequencia(contador_sequencia, moment, userid) VALUES(?, ?, ?);";
-        $timestamp = get_curr_timestamp();
-        $userid = get_logged_in_userid(); // TODO: null check
-        $sth = $dbh->prepare($query);
-        $sth->execute(array($cnt_seq, $timestamp, $userid));
-        
-        return $cnt_seq;
-}
 
 
 if (is_logged_in()) {
     show_header();
-    
+
     if(isset($_POST['page_name'])) {
         $page_name = $_POST['page_name'];
         $is_active = parse_is_active();
-        
+
         $dbh = get_database_handler();
         try {
-            $dbh->query(TRANSACTION_START);
+            $dbh->beginTransaction();
             $cnt_seq = update_sequencia_table($dbh);
             update_page_table($dbh, $page_name, $cnt_seq, $is_active);
-            $dbh->query(TRANSACTION_END);       
+            $dbh->commit();
         } catch (PDOException $e) {
             echo('<p>ERROR: {' . $e->getMessage() . '}</p>');
         }
- 
-        $dbh = null; 
-        
+
+        $dbh = null;
+
     }
 ?>
 
@@ -82,13 +61,13 @@ if (is_logged_in()) {
 <form method = "post" action = "<?php echo $_SERVER['PHP_SELF']; ?>">
 <fieldset>
     <label for="page_name">Name:</label>
-    <input type="text" id="page_name" name="page_name" value=" " /> <br />
+    <input type="text" id="page_name" name="page_name" value="" /> <br />
     <label for="is_active">Is active? (0 == FALSE | 1 == TRUE)</label>
     <input type="number" id="is_active" name="is_active" min="0" max="1" value="1"/>
     <input type="submit" name="submit" value="Add Page" />
 </fieldset>
 </form>
- 
+
 <?php
 } else {
     redirect_to_login();
